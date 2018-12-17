f8fd675147e6ea4583a38b93ac46a176bd9c905f
Administrator <Administrator@th002.shgw.local>
2018-12-17 15:07:15 +0800

    菜单配置

--- CIWTest/WebRoot/resources/js/index.js
+++ CIWTest/WebRoot/resources/js/index.js
@@ -7,6 +7,7 @@ $(function(){
 	$("#machinediv").hide();
 })
 
+
 function getUserInsframework(){
 	$.ajax({
 		type : "post",
@@ -17,6 +18,10 @@ function getUserInsframework(){
 		success : function(result){
 			var str = "<span>"+result.uname+"</span>";
 			$("#username").append(str);
+			for(var r=0;r<result.ary.length;r++){
+				resourceary.push(result.ary[r].resource);
+			}
+			anaylsis(result.ipurl);
 		},
 		error : function(errorMsg){
 			alert("数据请求失败，请联系系统管理员!");
@@ -24,238 +29,165 @@ function getUserInsframework(){
 	})
 }
 
-function openPersonChart(){
-	if($("#persondiv").is(":hidden")){
-	    $("#persondiv").slideDown();
-		$("#junctiondiv").slideUp();
-	    $("#machinediv").slideUp();
-		$("#personimg").css("transform","rotate(180deg)");
-		$("#junctionimg").css("transform","rotate(0deg)");
-		$("#machineimg").css("transform","rotate(0deg)");
+function openSubmenus(index1,index2){
+	$("#subimg"+index1+"_"+index2).css("transform","rotate(0deg)");
+	if($("#div"+index1+"_"+index2).is(":hidden")){
+		$("#div"+index1+"_"+index2).show();
 	}else{
-		$("#personimg").css("transform","rotate(0deg)");
-	    $("#persondiv").slideUp();
+		$("#div"+index1+"_"+index2).hide();
 	}
 }
 
-function openJunctionChart(){
-	if($("#junctiondiv").is(":hidden")){
-		$("#junctiondiv").slideDown();
-	    $("#persondiv").slideUp();
-	    $("#machinediv").slideUp();
-		$("#personimg").css("transform","rotate(0deg)");
-		$("#machineimg").css("transform","rotate(0deg)");
-		$("#junctionimg").css("transform","rotate(180deg)");
-	}else{
-		$("#junctiondiv").slideUp();
-		$("#junctionimg").css("transform","rotate(0deg)");
+function loadxmlDoc(file) {
+	try {
+		//IE
+		xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+	} catch (e) {
+		//Firefox, Mozilla, Opera, etc
+		xmlDoc = document.implementation.createDocument("", "", null);
 	}
-}
 
-function openMachineChart(){
-	if($("#machinediv").is(":hidden")){
-		$("#machinediv").slideDown();
-		$("#junctiondiv").slideUp();
-	    $("#persondiv").slideUp();
-		$("#personimg").css("transform","rotate(0deg)");
-		$("#junctionimg").css("transform","rotate(0deg)");
-		$("#machineimg").css("transform","rotate(180deg)");
-	}else{
-	    $("#machinediv").slideUp();
-		$("#machineimg").css("transform","rotate(0deg)");
+	try {
+		xmlDoc.async = false;
+		xmlDoc.load(file); //chrome没有load方法
+	} catch (e) {
+		//针对Chrome,不过只能通过http访问,通过file协议访问会报错
+		var xmlhttp = new window.XMLHttpRequest();
+		xmlhttp.open("GET", file, false);
+		xmlhttp.send(null);
+		xmlDoc = xmlhttp.responseXML.documentElement;
 	}
+	return xmlDoc;
 }
 
-
-function openFauit(){
-	addTab("故障报表","datastatistics/goFauit");
+var resourceary = [];
+function anaylsis(ipurl){
+	var object = loadxmlDoc(ipurl+"ConfigFile/menu.xml");
+	var menuinfo = object.getElementsByTagName("Menuinfo");
+	for(var m = 1; m <= menuinfo.length; m++){
+		for (var i = 0; i < menuinfo.length; i++) {
+			var showIndex = menuinfo[i].getElementsByTagName("ShowIndex");//显示位置
+			if (document.all) {
+				showIndex = showIndex[0].text;
+			} else {
+				showIndex = showIndex[0].textContent;
+			}
+			if(m == showIndex){
+				var menuName = menuinfo[i].getElementsByTagName("Name");//菜单名
+				var submenus = menuinfo[i].getElementsByTagName("Submenus");//二级菜单
+				var imgName = menuinfo[i].getElementsByTagName("ImgName");//菜单图标
+				var array = [], firstcontext = "";
+				if (document.all) {
+					menuName = menuName[0].text,imgName = imgName[0].text;
+				} else {
+					menuName = menuName[0].textContent,imgName = imgName[0].textContent;
+				}
+				var context = '<ul id="ul'+showIndex+'">';
+				for (var x = 0; x < submenus.length; x++) {
+					var firstName = submenus[x].getElementsByTagName("FirstName");
+					var firstResource = submenus[x].getElementsByTagName("FirstResource");
+					var firstimgName = submenus[x].getElementsByTagName("FirstImgName");//菜单图标
+					var firstsubmenus = submenus[x].getElementsByTagName("FirstSubmenus");//三级菜单
+					var firstshowIndex = submenus[x].getElementsByTagName("FirstShowIndex");//显示位置
+					var subnenustext,flag = true,lastcontext = "";
+					if (document.all) { //IE
+						firstName = firstName[0].text,firstResource = firstResource[0].text,firstimgName = firstimgName[0].text,
+						subnenustext = firstsubmenus[0].text,firstshowIndex = firstshowIndex[0].text;
+					} else {
+						firstName = firstName[0].textContent,firstResource = firstResource[0].textContent,firstimgName = firstimgName[0].textContent,
+						subnenustext = firstsubmenus[0].textContent,firstshowIndex = firstshowIndex[0].textContent;
+					}
+					if(subnenustext.trim()){
+						flag = false;
+						array.push(firstshowIndex);
+						firstcontext +='<li onclick="changeColor(this)" id="'+firstshowIndex+'"><a href="javascript:openSubmenus('+showIndex+','+firstshowIndex+')">'+
+						'<div><img src="resources/images/'+firstimgName+'" />&nbsp;&nbsp;'+firstName+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="resources/images/arrow.png" id="subimg'+showIndex+'_'+firstshowIndex+'"/></div>'+
+						'</a></li><li><div id="div'+showIndex+'_'+firstshowIndex+'" style="display:none;"><ul id="last'+firstshowIndex+'">';
+						for (var j = 0; j < firstsubmenus.length; j++) {
+							var LastName = firstsubmenus[j].getElementsByTagName("LastName");
+							var LastResource = firstsubmenus[j].getElementsByTagName("LastResource");
+							var LastimgName = firstsubmenus[j].getElementsByTagName("LastImgName");//菜单图标
+							var lastshowIndex = firstsubmenus[j].getElementsByTagName("LastShowIndex");//显示位置
+							if (document.all) { //IE
+								LastName = LastName[0].text,LastResource = LastResource[0].text,LastimgName = LastimgName[0].text,lastshowIndex = lastshowIndex[0].text;
+							} else {
+								LastName = LastName[0].textContent,LastResource = LastResource[0].textContent,LastimgName = LastimgName[0].textContent,lastshowIndex = lastshowIndex[0].textContent;
+							}
+							if(resourceary.indexOf(LastResource)!=-1){
+								lastcontext += '<li onclick="changeColor(this)" id="last'+lastshowIndex+'"><a href="javascript:openTab(\''+encodeURI(LastName)+'\',\''+LastResource+'\')" ><div><img src="resources/images/'+LastimgName+'" />&nbsp;&nbsp;'+LastName+'</div></a></li>';
+							}
+						}
+						firstcontext += lastcontext+'</ul></div></li>';
+						if(lastcontext == null || lastcontext == ""){
+							firstcontext = "";
+						}
+					}
+					if(flag){
+						if(resourceary.indexOf(firstResource)!=-1){
+							firstcontext +='<li onclick="changeColor(this)" id="'+firstshowIndex+'"><a href="javascript:openTab(\''+encodeURI(firstName)+'\',\''+firstResource+'\')" ><div><img src="resources/images/'+firstimgName+'" />&nbsp;&nbsp;'+firstName+'</div></a></li>';
+						}
+					}
+				}
+				context += firstcontext+'</ul>';
+				if(firstcontext != null && firstcontext != ""){
+					//动态渲染easyui的菜单样式
+					$('#accordiondiv').accordion('add', {
+						title : menuName,
+						content : context,
+						iconCls: imgName,
+						selected : false
+					});
+					menusort("ul"+showIndex,true);
+					for(var f=0;f<array.length;f++){
+						menusort("last"+array[f],false);
+					}
+				}
+				$("#accordiondiv").accordion({
+		            selected:0
+		        })
+			}
+		}
+	}
+	$.parser.parse($("#accordiondiv"));
 }
 
-function openItemData(){
-	addTab("班组生产数据","datastatistics/goItemData");
+//根据位置进行排序
+function menusort(name,flag){
+	var ul=document.getElementById(name);
+    var li=ul.getElementsByTagName("li");
+    var list=[];
+    //将li添加到list数组
+    for(var n=0;n<li.length;n++){
+    	var subid = li[n].id.substring(0, 4);
+    	if(flag){
+        	if(subid!="last"){
+            	list.push(li[n]);
+        	}
+    	}else{
+            list.push(li[n]);
+    	}
+    	
+    }
+    //对数组进行比较排序
+    list.sort(function (li1,li2){
+    	var n1,n2
+    	if(flag){
+    		n1=parseInt(li1.id);
+    		n2=parseInt(li2.id);
+    	}else{
+    		n1=parseInt(li1.id.substring(4));
+    		n2=parseInt(li2.id.substring(4));
+    	}
+        return n1-n2;
+    })
+    for(var x=0;x<list.length;x++){
+       ul.appendChild(list[x]);
+    }
 }
 
-function openMachineData(){
-	addTab("设备生产数据","datastatistics/goMachineData");
-}
-
-function openPersonData(){
-	addTab("人员生产数据","datastatistics/goPersonData");
-}
-
-function openWorkpieceData(){
-	addTab("工件生产数据","datastatistics/goWorkpieceData");
-}
-
-function openWeldItemData(){
-	addTab("班组焊接数据","datastatistics/goWeldItemData");
-}
-
-function openWeldMachineData(){
-	addTab("设备焊接数据","datastatistics/goWeldMachineData");
-}
-
-function openWeldPersonData(){
-	addTab("人员焊接数据","datastatistics/goWeldPersonData");
-}
-
-function opencatmail(){
-	addTab("CAT邮件管理","catweld/goCatMail");
-}
-
-function openCatWeld(){
-	addTab("CAT焊工管理","catweld/goCatWeld");
-
-}
-function opencatPerson(){
-	addTab("cat焊工合并版","welders/catWelder");
-}
-function opencatMachine(){
-	addTab("cat焊机合并版","weldingMachine/gocatMachine");
-}
-
-function openWeldWorkpieceData(){
-	addTab("工件焊接数据","datastatistics/goWeldWorkpieceData");
-}
-
-function openUser(){
-	addTab("用户管理","user/AllUser");
-}
-
-function openRole(){
-	addTab("角色管理","role/AllRole");
-}
-
-function openAuthority(){
-	addTab("权限管理","authority/AllAuthority");
-}
-
-function openResource(){
-	addTab("资源管理","resource/AllResource");
-}
-
-function openInsframework(){
-	addTab("组织机构管理","insframework/goInsframework");
-}
-
-function openDictionary(){
-	addTab("字典管理",'Dictionary/goDictionary');
-}
-
-function openCompanytEfficiency(){
-	addTab("焊工工效","companyChart/goCompanyEfficiency");
-}
-
-function openCompanyUse(){
-	addTab("单台设备运行数据统计","companyChart/goCompanyUse");
-}
-function openMachine(){
-	addTab("维修记录管理","maintain/goMaintain");
-}
-
-function openMaxMin(){
-	addTab("对照分析","companyChart/goContrast");
-}
-
-function openCompanyovertime(){
-	addTab("超时待机统计","companyChart/goCompanyOvertime");
-}
-function openCompanyIdle(){
-	addTab("设备闲置率","companyChart/goCompanyIdle");
-}
-function openWeldParameter(){
-	addTab("焊接参数维度表","rep/weldpara");
-}
-
-function openWireUse(){
-	addTab("焊丝用量维度表","rep/wireuse");
-}
-
-function openWelderReport(){
-	addTab("焊工维度表","rep/welderreport");
-}
-
-function openParameter(){
-	addTab("参数设置","Dictionary/goParameter");
-}
-
-function openProduct(){
-	addTab("产品管理","product/AllProduct");
-}
-
-function openWps(){
-	addTab("工艺管理","wps/AllWps");
-}
-
-function openSpe(){
-	addTab("规范管理","wps/AllSpe");
-}
-
-function openWeldingmachineMax(){
-	addTab("焊机工时最高","companyChart/goCompanyWmMax");
-}
-
-function openWeldingmachineMin(){
-	addTab("焊机工时最低","companyChart/goCompanyWmMin");
-}
-
-function openWelderMax(){
-	addTab("焊工工时最高","companyChart/goCompanyWelderMax");
-}
-
-function openWelderMin(){
-	addTab("焊工工时最低","companyChart/goCompanyWelderMin");
-}
-
-function openWeldingMachine(){
-	addTab("CAT焊机设备管理","weldingMachine/goWeldingMachine");
-}
-
-function openWeldingMachines(){
-	addTab("焊机设备管理","weldingMachine/goWeldingMachines");
-}
-function openWedJunction(){
-	addTab("焊缝管理","weldedjunction/goWeldedJunction");
-}
-
-function openPerson(){
-	addTab("焊工管理","welders/AllWelder");
-}
-
-function openGather(){
-	addTab("采集模块管理","gather/goGather");
-}
-
-function openCompanyHour(){
-	addTab("焊缝焊接工时","companyChart/goCompanyHour");
-}
-
-function openCompanyoverproof(){
-	addTab("焊接工艺超标","companyChart/goCompanyOverproof");
-}
-
-function openCompanyLoads(){
-	addTab("设备负荷率","companyChart/goCompanyLoads");
-}
-
-function openCompanyNoLoads(){
-	addTab("设备空载率","companyChart/goCompanyNoLoads");
-}
-
-function openCompanyTd(){
-	window.open("td/AllTd");
-}
-
-function openHistory(){
-	addTab("历史曲线","rep/history");
-}
-
-function openHistoryJunction(){
-	addTab("历史曲线.","rep/historyJunction");
-}
-
-function openAndroid(){
-	addTab("安卓上传数据统计","rep/android");
+//打开标签页
+function openTab(name,resource){
+	addTab(name,resource);
 }
 
 function addWelcom(title,url){
